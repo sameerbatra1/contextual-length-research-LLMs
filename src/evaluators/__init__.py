578e5b555eae2f823983b72c36b2fc994685dd71
Sameer Batra <sameerbatra1@gmail.com>
2025-12-23 22:21:20 +0000

    Add __init__.py to evaluators module: - Imported BaseEvaluator, NeedleHaystackEvaluator, QualityEvaluator, and PerplexityEvaluator - Defined __all__ for public API exposure

---
+++ src/evaluators/__init__.py
@@ -0,0 +1,12 @@
+from .base_evaluator import BaseEvaluator
+from .needle_haystack import NeedleHaystackEvaluator
+from .quality import QualityEvaluator
+from .perplexity import PerplexityEvaluator
+
+__all__ = [
+    'BaseEvaluator',
+    'NeedleHaystackEvaluator', 
+    'QualityEvaluator',
+    'PerplexityEvaluator'
+]
+
